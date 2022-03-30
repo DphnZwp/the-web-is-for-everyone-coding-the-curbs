eca41b2a4a47e186e1e9c4f4f33a49c8468cfe6d
DphnZwp <zwuupdaphne@gmail.com>
2022-03-31 01:05:08 +0200

    Data van api in tabellen

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args))
 
 // Serve public files
 app.use(express.static('public'))
@@ -11,14 +12,22 @@ app.use('/assets', express.static('assets'))
 
 app.get('/', (req, res) => {
   // res.send('Hallo wereld!')
-  res.render('index', {
-    title: 'Smart Zones',
+  fetchJson('https://codingthecurbs.api.fdnd.nl/v1/smartzone').then(function (jsonData) {
+    res.render('index', {
+      title: 'Smart Zones',
+      smartzones: jsonData.data,
+    })
   })
 })
-
 
 app.set('port', process.env.PORT || 9000)
 
 const server = app.listen(app.get('port'), () => {
   console.log(`Application started on port: ${app.get('port')}`)
 })
+
+async function fetchJson(url) {
+  return await fetch(url)
+    .then((response) => response.json())
+    .catch((error) => error)
+}
