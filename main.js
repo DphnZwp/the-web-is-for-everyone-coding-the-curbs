fef517cb953426dae0f96d64deacad4957cad51e
DphnZwp <zwuupdaphne@gmail.com>
2022-04-04 14:00:08 +0200

    Links in een select

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const port = process.env.PORT || 9000
 const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args))
 
 // Serve public files
@@ -12,10 +13,20 @@ app.use('/assets', express.static('assets'))
 app.use('/assets/icons', express.static('icons'))
 
 app.get('/', (req, res) => {
-  // res.send('Hallo wereld!')
   fetchJson('https://codingthecurbs.api.fdnd.nl/v1/smartzone').then(function (jsonData) {
     res.render('index', {
       title: 'Smart Zones',
+      smartzones: jsonData.data,
+    })
+  })
+})
+
+app.get('/smartzones', (request, response) => {
+  fetchJson('https://codingthecurbs.api.fdnd.nl/v1/smartzone').then(function (
+    jsonData
+  ) {
+    response.render('smartzones', {
+      title: 'Alle smartzones',
       smartzones: jsonData.data,
     })
   })
@@ -33,7 +44,27 @@ app.get('/smartzone/:smartzoneId', (request, response) => {
   })
 })
 
-const port = process.env.PORT || 9000
+app.get('/smartzone/tijn', (request, response) => {
+  fetchJson('https://codingthecurbs.api.fdnd.nl/v1/smartzone').then(function (
+    jsonData
+  ) {
+    response.render('tijn', {
+      title: 'Dit is een enkele smartzone',
+      smartzones: jsonData.data,
+    })
+  })
+})
+
+app.get('/smartzone/sarah', (request, response) => {
+  fetchJson('https://codingthecurbs.api.fdnd.nl/v1/smartzone').then(function (
+    jsonData
+  ) {
+    response.render('sarah', {
+      title: 'Smartzones',
+      smartzones: jsonData.data,
+    })
+  })
+})
 
 const server = app.listen(port, () => {
   console.log(`Application started on port: ${port}`)
